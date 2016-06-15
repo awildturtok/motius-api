d9cb98741171d633b9a57e32d40dfc52e7c1bb05
Fabian Kovacs <fabian.kovacs@koxi.de>
2016-06-15 14:56:34 +0200

    moved to pug templates, fixed public path, added process title

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -10,9 +11,11 @@ var users = require('./routes/users');
 
 var app = express();
 
+process.title = 'motius-api';
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,7 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.use('/', routes);
 app.use('/users', users);
