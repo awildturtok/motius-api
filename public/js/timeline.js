1a8522021afd0d12345bcc41ae336c3e144a4af9
Fabian Kovacs <fabian.kovacs@koxi.de>
2016-06-15 14:58:08 +0200

    Implemented timeline etc

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -1,1 +1,37 @@
+$(function()
+{
+    $(".timeline").slideUp();
 
+    $(".project").each(function()
+    {
+        let milestones = $(this).data('project').milestones;
+
+        if(milestones.length == 0)
+            return;
+
+        $(this).addClass('clickable');
+
+        let items = new vis.DataView(
+            new vis.DataSet(milestones)
+            , {
+                fields:
+                {
+                    'id' : 'id',
+                    'name' : 'content',
+                    'start_date' : 'start',
+                    'end_date' : 'end'
+                }
+            }
+        );
+
+        let container = $(this).find('.timeline');
+
+        let timeline = new vis.Timeline(container[0], items.get(), { });
+    });
+
+    $(".project").click(function ()
+    {
+        $(".timeline").slideUp();
+        $(this).find(".timeline").slideDown();
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
+require('babel-register');
+
 var express = require('express');
+
+var rp = require('request-promise');
+var _ = require('highland');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    rp('https://www.motius.de/api/usecases/?format=json' , {json : true})
+
+        .then((data) => res.render('index', { title: 'Motius API Timeline', contents : data}))
+
+        .catch((err) => res.status(503).send('Failed to connect to Motius Server'));
+
 });
 
 module.exports = router;
